569b3235533ec927e5a73239e8b34d9cde8cf3ad
LUsername <492254446@qq.com>
2019-11-25 17:38:28 +0800

    Tabbar 组件

--- src/components/tabbar/index.vue
+++ src/components/tabbar/index.vue
@@ -1,13 +1,33 @@
 <template>
-    $END$
+    <div class="g-tabbar">
+      <router-link class="g-tabbar-item" to="/home">
+        <i class="iconfont icon-home"></i>
+        <span>首页</span>
+      </router-link>
+      <router-link class="g-tabbar-item" to="/category">
+        <i class="iconfont icon-category"></i>
+        <span>分类页</span>
+      </router-link>
+      <router-link class="g-tabbar-item" to="/cart">
+        <i class="iconfont icon-cart"></i>
+        <span>购物车</span>
+      </router-link>
+      <router-link class="g-tabbar-item" to="/personal">
+        <i class="iconfont icon-personal"></i>
+        <span>个人中心</span>
+      </router-link>
+    </div>
 </template>
 
 <script>
   export default {
-    name: 'index'
-  }
+    name: 'CTabbar'
+  };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import '~assets/scss/mixins';
+  .link-active-color{
+    color: $link-active-color;
+  }
 </style>
