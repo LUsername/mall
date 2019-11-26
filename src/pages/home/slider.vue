c7cc3b87d60793b3083ce6de4f7698f152136ba8
LUsername <492254446@qq.com>
2019-11-26 15:30:47 +0800

    幻灯片组件--从服务器端获取数据

--- src/pages/home/slider.vue
+++ src/pages/home/slider.vue
@@ -5,7 +5,9 @@
             :loop="loop"
             :interval="interval"
             :pagination="pagination"
+            v-if="sliders.length"
         >
+        <!-- 添加v-if="sliders.length"避免未获取数据就开始渲染幻灯片组件 -->
             <swiper-slide v-for="(item,index) in sliders" :key="index">
                 <a :href="item.linkUrl" class="slider-link">
                     <img :src="item.picUrl" class="slider-img">
@@ -19,6 +21,7 @@
 import MeSlider from 'base/slider';
 import {swiperSlide} from 'vue-awesome-swiper';
 import {sliderOptions} from './config';
+import {getHomeSlider} from 'api/home';
 export default {
     name:'HomeSlider',
     components:{
@@ -31,23 +34,17 @@ export default {
             loop: sliderOptions.loop,
             interval: sliderOptions.interval,
             pagination: sliderOptions.pagination,
-            sliders:[
-                {
-                 'linkUrl':'https://www.imooc.com',
-                 'picUrl':require('./1.jpg')
-                },
-                {
-                 'linkUrl':'https://www.imooc.com',
-                 'picUrl':require('./2.jpg')
-                },
-                {
-                 'linkUrl':'https://www.imooc.com',
-                 'picUrl':require('./3.jpg')
-                },{
-                 'linkUrl':'https://www.imooc.com',
-                 'picUrl':require('./4.jpg')
-                }
-            ]
+            sliders:[]
+        }
+    },
+    created(){
+        this.getSliders();
+    },
+    methods:{
+        getSliders(){
+            getHomeSlider().then(data => {
+                this.sliders = data;
+            })
         }
     }
 }
