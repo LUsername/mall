ae38f1e30c93dade0a84fb49736c46471c65c6cd
LUsername <492254446@qq.com>
2019-12-07 14:16:37 +0800

    实现分类页Header和侧栏

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,13 +1,63 @@
 <template>
-    <div>Category</div>
+  <div class="category">
+    <header class="g-header-container">
+      <category-header/>
+    </header>
+    <div class="g-content-container">
+      <div class="sidebar">
+        <CategoryTab @switch-tab="getCurrentId"/>
+      </div>
+      <div class="main">
+        <CategoryContent :curId="curId"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name:'Category'
-}
+  import CategoryHeader from './header';
+  import CategoryTab from './tab';
+  import CategoryContent from './content';
+
+  export default {
+    name: 'Category',
+    components: {
+      CategoryHeader,
+      CategoryTab,
+      CategoryContent
+    },
+    data(){
+      return{
+        curId:''
+      }
+    },
+    methods:{
+      getCurrentId(id){
+        this.curId = id;
+      }
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
+  @import "~assets/scss/mixins";
 
+  .category {
+    overflow:hidden;
+    width:100%;
+    height:100%;
+    background-color: $bgc-theme;
+  }
+
+  .g-content-container {
+    display: flex;
+  }
+  .sidebar {
+    width: 80px;
+    height: 100%;
+  }
+  .main {
+    flex: 1;
+    height: 100%;
+  }
 </style>
